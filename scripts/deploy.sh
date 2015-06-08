876b90d03dde3153f2ea398b17e5f38e9bee8a00
Koen Vendrik <k.vendrik@gmail.com>
2015-06-08 17:13:33 +0200

    Changes start flag name to restart

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,6 +1,6 @@
 printf '\n>> Copying\n'
 rsync -avz -e "ssh -o StrictHostKeyChecking=no -o UserKnownHostsFile=/dev/null" --progress ./ root@37.128.149.176:/var/www/html/form-and-function/ --exclude '.git' --exclude 'node_modules'
 
-if [ "$1" == "--start" ]; then
+if [ "$1" == "--restart" ]; then
     ssh root@37.128.149.176 'cd /var/www/html/form-and-function && sh scripts/setup.sh && sh scripts/start-server.sh'
 fi
